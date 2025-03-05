8b180e12aefc3e1d4f918caec5b9ef7b8639ce6d
NovaStarmax <a.gobbe13@gmail.com>
2025-03-05 16:51:28 +0100

    Add pictures to practise deep learning

---
+++ deep_learning/utilities.py
@@ -0,0 +1,16 @@
+import h5py
+import numpy as np
+import os
+
+
+def load_data():
+    train_dataset = h5py.File('datasets/trainset.hdf5', "r") # 'datasets/trainset.hdf5'
+    X_train = np.array(train_dataset["X_train"][:]) # your train set features
+    y_train = np.array(train_dataset["Y_train"][:]) # your train set labels
+
+    test_dataset = h5py.File('datasets/testset.hdf5', "r") # 'datasets/testset.hdf5'
+    X_test = np.array(test_dataset["X_test"][:]) # your train set features
+    y_test = np.array(test_dataset["Y_test"][:]) # your train set labels
+    
+    return X_train, y_train, X_test, y_test
+
